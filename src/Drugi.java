2783b7257701839d70a5ebacb598da64badbc921
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-01 19:10:29 +0200

    Zduplikowałem powitanie imienne

--- src/Drugi.java
+++ src/Drugi.java
@@ -2,5 +2,6 @@ public class Drugi {
     public static void main(String[] args) {
         System.out.println("Jestem w drugim programie");
         System.out.println("Witaj Bartoszu!");
+        System.out.println("Witaj Bartoszu!");
     }
 }
