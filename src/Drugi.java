8a266559a9f9a6a6169a97a7ef825d1bbe473237
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-01 19:08:13 +0200

    Zmieniłem imie

--- src/Drugi.java
+++ src/Drugi.java
@@ -1,6 +1,6 @@
 public class Drugi {
     public static void main(String[] args) {
         System.out.println("Jestem w drugim programie");
-        System.out.println("Witaj Bartosz!");
+        System.out.println("Witaj Bartoszu!");
     }
 }
